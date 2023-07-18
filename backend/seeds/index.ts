c90685d1db3318dda030a41067dc6cfcc82a804f
onelky <onelky.hernandez29@gmail.com>
2023-07-18 19:49:55 -0400

    add common methods to campgrounds test file and refactor seedDB

--- backend/seeds/index.ts
+++ backend/seeds/index.ts
@@ -1,9 +1,13 @@
-import * as process from 'process'
-import mongoose from 'mongoose'
+import mongoose, { Document } from 'mongoose'
+import * as dotenv from 'dotenv'
 import Campground from '../models/campground'
 import { ConnectOptions } from 'mongoose'
+import User from '../models/user'
+const process = require('process')
 const { places, descriptors } = require('./seedHelper')
 const cities = require('./cities')
+
+dotenv.config()
 
 mongoose.connect(process.env.DATABASE_URL as string, { useNewUrlParser: true } as ConnectOptions)
 
@@ -16,16 +20,22 @@ db.once('open', () => {
 const sample = (array: Array<any>) => array[Math.floor(Math.random() * array.length)]
 
 const seedDB = async () => {
+    const user = (await User.findOne({ username: 'onelky' })) as unknown as Document
     await Campground.deleteMany({})
-    for (let i = 0; i < 10; i++) {
+
+    console.log('Creating Campgrounds...')
+    for (let i = 0; i < 2; i++) {
         const randomCityIndex = Math.floor(Math.random() * 1000)
         const city = cities[randomCityIndex]
+
         const newCamp = new Campground({
+            author: user._id,
             location: `${city.city}, ${city.state}`,
             title: `${sample(descriptors)} ${sample(places)}`
         })
         await newCamp.save()
     }
+    console.log('Finished creation of Campgrounds...')
 }
 
 seedDB().then(() => {
