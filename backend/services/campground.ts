4bfdfbb4f7bc421ff52ebf81c6f77475aa63a05d
onelky <onelky.hernandez29@gmail.com>
2023-07-21 15:45:31 -0400

    add functionality of removing images from campground

--- backend/services/campground.ts
+++ backend/services/campground.ts
@@ -1,5 +1,6 @@
 import Campground, { CampgroundDocument, ICampground } from '../models/campground'
-
+import isEmpty from 'lodash/isEmpty'
+import cloudinary from '../cloudinaryConfig'
 export const findAllCampgrounds = async (): Promise<ICampground[]> => await Campground.find({})
 
 export const createCampground = async (data: ICampground, author: string): Promise<CampgroundDocument> => {
@@ -15,7 +16,13 @@ export const findCampground = async (id: string): Promise<CampgroundDocument> =>
 }
 
 export const updateCampground = async (id: string, data: Partial<ICampground>): Promise<CampgroundDocument> => {
-    return (await Campground.findByIdAndUpdate(id, data, { runValidators: true, returnDocument: 'after' })) as CampgroundDocument
+    const { images, deleteImages, ...update } = data
+    if (!isEmpty(deleteImages)) deleteImages?.forEach((filename) => cloudinary.uploader.destroy(filename))
+    return (await Campground.findByIdAndUpdate(
+        id,
+        { ...update, ...(images && { $push: { images } }), ...(deleteImages && { $pull: { images: { filename: { $in: deleteImages } } } }) },
+        { runValidators: true, returnDocument: 'after' }
+    )) as CampgroundDocument
 }
 export const deleteCampground = async (id: string): Promise<CampgroundDocument> => {
     return (await Campground.findByIdAndDelete(id)) as CampgroundDocument
