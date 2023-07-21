1f47d5d853f85c23bbda025af49438f7c6c20193
onelky <onelky.hernandez29@gmail.com>
2023-07-21 14:04:52 -0400

    add email validation to user zod schema

--- backend/schemas/user.ts
+++ backend/schemas/user.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const userSchema = z
     .object({
-        email: z.string({ required_error: 'Email is required', invalid_type_error: 'Email must be a string' }).nonempty(),
+        email: z.string({ required_error: 'Email is required', invalid_type_error: 'Email must be a string' }).email().nonempty(),
         username: z.string({ required_error: 'Username is required' }).nonempty(),
         password: z.string({ required_error: 'Password is required' }).nonempty()
     })
