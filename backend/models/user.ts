407f184ad6fcb0047fad6788cd83a8802cc6d20f
onelky <onelky.hernandez29@gmail.com>
2023-07-24 20:23:51 -0400

    include google authentication strategy
    * install passport-google-oidc package
    * add GoogleStrategy to passport
    * include '/auth/google' and '/auth/google/callback' routes to users endpoint
    * add method property to IUser interface
    * create IUserGoogleAuth interface
    * add registerUser method and update createUser
    * add findUser to usersService

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -2,11 +2,18 @@ import { model, Schema, PassportLocalDocument } from 'mongoose'
 import passportLocalMongoose from 'passport-local-mongoose'
 import { isEmail } from 'validator'
 
+export enum AuthMethods {
+    google = 'google',
+    local = 'local'
+}
 export interface IUser {
     email: string
     username: string
     password: string
+    method?: AuthMethods
 }
+
+export interface IUserGoogleAuth extends Omit<IUser, 'password'> {}
 
 export interface UserDocument extends IUser, PassportLocalDocument {
     hash: string
@@ -14,6 +21,11 @@ export interface UserDocument extends IUser, PassportLocalDocument {
 }
 
 const UserSchema = new Schema<UserDocument>({
+    username: { type: String, required: true, unique: true },
+    hash: { type: String },
+    salt: { type: String },
+    password: { type: String },
+    method: { type: String, enum: AuthMethods, required: true, default: AuthMethods.local },
     email: {
         type: String,
         required: true,
@@ -22,11 +34,7 @@ const UserSchema = new Schema<UserDocument>({
             validator: isEmail,
             message: 'Invalid email'
         }
-    },
-    username: { type: String, required: true, unique: true },
-    password: { type: String },
-    hash: { type: String },
-    salt: { type: String }
+    }
 })
 
 declare global {
